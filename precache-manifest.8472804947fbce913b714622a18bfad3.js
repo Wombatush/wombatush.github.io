ccf15a6866e3e17bbdb027dd972855bde7fd470e
Evgeny Nazarov <e.v.nazarov@mail.ru>
2020-04-28 11:57:40 +1000

    Updates

---
+++ precache-manifest.8472804947fbce913b714622a18bfad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4272237ee9a9a51c7a26bd2b57e18b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41479e508ec9ef4f1b62",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b442e4b25d91476658a2",
+    "url": "/static/js/2.e7933055.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7933055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41479e508ec9ef4f1b62",
+    "url": "/static/js/main.cef17265.chunk.js"
+  },
+  {
+    "revision": "ec9996b0ee47c0746db8",
+    "url": "/static/js/runtime-main.1ac3eaec.js"
+  }
+]);
